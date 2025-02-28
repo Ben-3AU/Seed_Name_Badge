8e4d4cc91d8d44f0834c54b12b145905d2ce5b02
Ben Sweet <hello@terratag.com.au>
2025-02-28 20:01:02 +1000

    security: remove redundant CORS middleware and enforce strict origin whitelist

--- server.js
+++ server.js
@@ -61,21 +61,6 @@ app.use(cors({
 // Serve static files
 app.use(express.static(path.join(__dirname)));
 
-// Add CORS headers to all responses
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header('Access-Control-Allow-Credentials', true);
-    
-    // Handle preflight requests
-    if (req.method === 'OPTIONS') {
-        res.sendStatus(200);
-    } else {
-        next();
-    }
-});
-
 // Stripe configuration endpoint
 app.get('/config', (req, res) => {
     if (!process.env.STRIPE_PUBLISHABLE_KEY) {
